d1eaf1a8fbc10927b929c65ade24e157a9d98215
soumyacoder19 <soumyagrover19@gmail.com>
2026-07-03 13:37:01 +0530

    Add files via upload

---
+++ user.module.ts
@@ -0,0 +1,17 @@
+﻿import { Module, forwardRef } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { User } from './user.entity';
+import { SessionModule } from '../session/session.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    forwardRef(() => SessionModule),
+  ],
+  controllers: [UserController],
+  providers: [UserService],
+  exports: [UserService, TypeOrmModule],
+})
+export class UserModule {}
